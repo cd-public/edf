d25b5f0f0c6998d6daa82e5f488b9b81479e2224
Lee A Barnett <lbarnett@cs.unc.edu>
2017-05-08 16:35:31 -0400

    remove non project related

--- model/time.v
+++
@@ -1,6 +0,0 @@
-Module Time.
-
-  (* Time is defined as a natural number. *)
-  Definition time := nat.
-
-End Time.
--- util/divround.v
+++
@@ -1,4 +0,0 @@
-From mathcomp Require Import ssrbool ssrnat div.
-
-Definition div_floor (x y: nat) : nat := x %/ y.
-Definition div_ceil (x y: nat) := if y %| x then x %/ y else (x %/ y).+1.
